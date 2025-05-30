0c60fbd3b73830b732f50c4d11114fed7df363e2
Lucif3r1101 <55076657+Lucif3r1101@users.noreply.github.com>
2025-05-30 21:54:06 +0530

    version bumo to v0.1.9

--- Formula/ubl.rb
+++ Formula/ubl.rb
@@ -1,7 +1,7 @@
 class Ubl < Formula
   desc "Universal Binary Locker CLI"
   homepage "https://github.com/Lucif3r1101/ubl"
-  url "https://github.com/Lucif3r1101/ubl/releases/download/v0.1.2/ubl-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/Lucif3r1101/ubl/releases/download/v0.1.9/ubl-x86_64-apple-darwin.tar.gz"
   sha256 "5c4eef250a75829886593a0741294866bb922e922529d2a7601cbc7d176f8dc9"
   version "0.1.9"
 

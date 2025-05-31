1f6a7f72a1f794d8307b4a06102b77c1d0f3de26
Lucif3r1101 <55076657+Lucif3r1101@users.noreply.github.com>
2025-05-31 22:21:29 +0530

    Update ubl.rb to 0.27

--- Formula/ubl.rb
+++ Formula/ubl.rb
@@ -1,9 +1,9 @@
 class Ubl < Formula
   desc "Universal Binary Locker CLI"
   homepage "https://github.com/Lucif3r1101/ubl"
-  url "https://github.com/Lucif3r1101/ubl/releases/download/v0.2.1/ubl-x86_64-apple-darwin.tar.gz"
-  sha256 "dbeb8c2054d8d3575c06a86af4e96bc087ac6fe685fb9c780037a2fa39206292"
-  version "0.2.1"
+  url "https://github.com/Lucif3r1101/ubl/releases/download/v0.2.7/ubl-x86_64-apple-darwin.tar.gz"
+  sha256 "e4dfbd0cd3fef25bf9a9cff25e9a1989aea1eb952314ef02d1154ad59cbc3f3e"
+  version "0.2.7"
 
   def install
     bin.install "ubl"
